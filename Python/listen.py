0e84d31ee0bf648d7b8c69f33af9aee4205ded2d
pk1on <92085410+pk1on@users.noreply.github.com>
2023-03-08 11:12:32 +0100

    Add files via upload

---
+++ Python/listen.py
@@ -0,0 +1,11 @@
+meinBeispiel = []
+ele = int(input("Anzahl Elemente:"))
+
+for i in range(ele):
+    meinBeispiel.append(input("Element:"))
+
+meinBeispiel.
+
+print(meinBsp)
+
+# print(meinBeispiel[3]) gibt das vierte Element der Liste meinBeispiel aus.
